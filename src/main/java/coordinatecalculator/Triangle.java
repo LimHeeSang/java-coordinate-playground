17621d0f726a73321f8b539229941464c7738f4f
heesang <dlagmltkd12@naver.com>
2022-03-27 04:16:14 +0900

    feat(coordinatecalculator) : 삼각형의 넓이를 계산하는 기능 구현

---
+++ src/main/java/coordinatecalculator/Triangle.java
@@ -0,0 +1,30 @@
+package coordinatecalculator;
+
+public class Triangle {
+    private final CoordinatePair point1;
+    private final CoordinatePair point2;
+    private final CoordinatePair point3;
+
+    public Triangle(CoordinatePair point1, CoordinatePair point2, CoordinatePair point3) {
+        this.point1 = point1;
+        this.point2 = point2;
+        this.point3 = point3;
+    }
+
+    public double calculateArea() {
+        double a = point1.calculateLineLength(point2);
+        double b = point2.calculateLineLength(point3);
+        double c = point3.calculateLineLength(point1);
+        double s = (a + b + c) / 2;
+
+        return getResult(a, b, c, s);
+    }
+
+    /**
+     *  result = root(s(s - a)(s - b)(s - c))
+     */
+    private double getResult(double a, double b, double c, double s) {
+        double result = Math.sqrt(s * (s - a) * (s - b) * (s - c));
+        return Math.round(result);
+    }
+}
---
+++ src/test/java/coordinatecalculator/TriangleTest.java
@@ -0,0 +1,16 @@
+package coordinatecalculator;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TriangleTest {
+    @Test
+    void calculate_area() {
+        Triangle triangle = new Triangle(
+                new CoordinatePair(10, 10),
+                new CoordinatePair(14, 15),
+                new CoordinatePair(20, 8)
+        );
+        Assertions.assertThat(triangle.calculateArea()).isEqualTo(29.0D);
+    }
+}
