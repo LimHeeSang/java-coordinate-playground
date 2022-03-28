ab6950aefa40f1d6d227031374c4018289236dd1
heesang <dlagmltkd12@naver.com>
2022-03-29 04:26:03 +0900

    refactor(coordinatecalculator) : 좌표를 리스트로 받을 수 있게 생성자 리팩토링

--- src/main/java/coordinatecalculator/model/Line.java
+++ src/main/java/coordinatecalculator/model/Line.java
@@ -1,15 +1,15 @@
 package coordinatecalculator.model;
 
-public class Line {
-    private final Point point1;
-    private final Point point2;
+import java.util.List;
 
-    public Line(Point point1, Point point2) {
-        this.point1 = point1;
-        this.point2 = point2;
+public class Line {
+    private final List<Point> points;
+
+    public Line(List<Point> points) {
+        this.points = points;
     }
 
     public double calculateLength() {
-        return point1.calculateLength(point2);
+        return points.get(0).calculateLength(points.get(1));
     }
 }
--- src/test/java/coordinatecalculator/model/LineTest.java
+++ src/test/java/coordinatecalculator/model/LineTest.java
@@ -3,10 +3,14 @@ package coordinatecalculator.model;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class LineTest {
     @Test
     void calculateLength() {
-        Line line = new Line(new Point(10, 10), new Point(14, 15));
+        List<Point> points = Arrays.asList(new Point(10, 10), new Point(14, 15));
+        Line line = new Line(points);
         double length = line.calculateLength();
         Assertions.assertThat(length).isEqualTo(6.403124D, Assertions.offset(0.00099));
     }
