4b27dca453d5a9cf39a5bc7ad9b16c512aa2da6c
heesang <dlagmltkd12@naver.com>
2022-03-31 01:12:47 +0900

    feat(coordinatecalculator) : 프로그램을 실행하는 메인클래스 구현

---
+++ src/main/java/coordinatecalculator/CoordinateMain.java
@@ -0,0 +1,9 @@
+package coordinatecalculator;
+
+import coordinatecalculator.controller.CoordinateCalculator;
+
+public class CoordinateMain {
+    public static void main(String[] args) {
+        new CoordinateCalculator().run();
+    }
+}
